26df148c96c60b2e1bc3be4f4bdeeb808f9f77a1
Aloysius Teo <aloysiusteo72@gmail.com>
2023-02-16 05:04:32 +0000

    Update 6 files

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,31 +1,39 @@
-import { useWeb3Modal } from "@web3modal/react"
-import { useState } from "react"
-import { useAccount, useDisconnect } from "wagmi"
+import { useWeb3Modal } from "@web3modal/react";
+import { useState } from "react";
+import { useAccount, useDisconnect } from "wagmi";
+import { Button } from "@nextui-org/react";
 
 export default function CustomButton() {
-  const [loading, setLoading] = useState(false)
-  const { open } = useWeb3Modal()
-  const { isConnected } = useAccount()
-  const { disconnect } = useDisconnect()
-  const label = isConnected ? "Disconnect" : "Connect Custom"
+  const [loading, setLoading] = useState(false);
+  const { open } = useWeb3Modal();
+  const { isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const label = isConnected ? "Disconnect" : "Connect Custom";
 
   async function onOpen() {
-    setLoading(true)
-    await open()
-    setLoading(false)
+    setLoading(true);
+    await open();
+    setLoading(false);
   }
 
   function onClick() {
     if (isConnected) {
-      disconnect()
+      disconnect();
     } else {
-      onOpen()
+      onOpen();
     }
   }
 
   return (
-    <button onClick={onClick} disabled={loading}>
+    <Button
+      bordered
+      color="gradient"
+      auto
+      shadow
+      onClick={onClick}
+      disabled={loading}
+    >
       {loading ? "Loading..." : label}
-    </button>
-  )
+    </Button>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -62,6 +62,28 @@ const wagmiClient = createClient({
   }),
 
   provider,
+  explorerAllowList: [
+    // rainbow
+    "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369",
+    // Metamask
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+    // Unstoppable Domains
+    "8308656f4548bb81b3508afe355cfbb7f0cb6253d1cc7f998080601f838ecee3",
+    //Unstoppable Wallet
+    "7e90b95230bc462869bbb59f952273d89841e1c76bcc5319898e08c9f34bd4cd",
+    //OpenSea
+    "f759efd17edb158c361ffd793a741b3518fe85b9c15d36b9483fba033118aaf2",
+  ],
+  desktopWallets: [
+    {
+      id: "8308656f4548bb81b3508afe355cfbb7f0cb6253d1cc7f998080601f838ecee3",
+      name: "Unstoppable",
+      links: {
+        native: "string",
+        universal: "string",
+      },
+    },
+  ],
 });
 
 // 3. Configure modal ethereum client
@@ -83,7 +105,16 @@ export default function App({ Component, pageProps }) {
         </WagmiConfig>
       ) : null}
 
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        themeMode="light"
+        themeColor="purple"
+        themeBackground="gradient | themeColor"
+        termsOfServiceUrl="https://example.com/terms-and-conditions"
+        privacyPolicyUrl="https://example.com/privacy-policy"
+    
+      />
     </>
   );
 }
